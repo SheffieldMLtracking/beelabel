4280e83c807ca0531c997ac770559294506d8c8d
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-03-08 16:48:40 +0000

    test: Add grab_photos_in_timerange test and fixture

--- tests/test_alignment.py
+++ tests/test_alignment.py
@@ -1,13 +1,53 @@
+"""
+Unit tests for the code in beelabel/alignment.py
+"""
+
+import datetime
+import logging
+import time
+import tempfile
+from pathlib import Path
+
+import pytest
+
 import beelabel.alignment
 
 
-def test_grab_photos_in_timerange():
-    # TODO generate empty files in a temp dir
+class TestPhotosInTimeRange:
+    """
+    Tests for the grab_photos_in_timerange() function
+    """
 
-    beelabel.alignment.grab_photos_in_timerange(
-        path='photos/system001',
-        starttime='08:00:00',
-        endtime='08:10:00'
-    )
+    @staticmethod
+    @pytest.fixture(scope='class', autouse=True)
+    def photo_dir():
+        with tempfile.TemporaryDirectory() as temp_dir:
+            timestamp = datetime.datetime.now()
 
-    # TODO delete temp files
+            for i in range(3):
+                timestamp + datetime.timedelta(hours=i)
+                # TODO revert to colons '%H:%M:%S'
+                time_of_day = timestamp.time().strftime('%H-%M-%S')
+                filename = f"file_{i}_{time_of_day}.np"
+                path = Path(temp_dir).joinpath(filename)
+                path.touch(exist_ok=True)
+
+            yield path
+
+    def test_grab_photos_in_timerange_str(self, photo_dir):
+        # TODO generate empty files in a temp dir
+
+        beelabel.alignment.grab_photos_in_timerange(
+            path=str(photo_dir),
+            starttime='08:00:00',
+            endtime='08:10:00'
+        )
+
+        # TODO delete temp files
+
+    def test_grab_photos_in_timerange_struct_time(self, photo_dir):
+        beelabel.alignment.grab_photos_in_timerange(
+            path=str(photo_dir),
+            starttime=time.strptime('08:00:00', '%H:%M:%S'),
+            endtime=time.strptime('08:10:00', '%H:%M:%S')
+        )
